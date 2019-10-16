acdcb15c357d3a99f9a9dbbab91e3ee62960b087
ferencz izabella <ferencz_bella@yahoo.com>
2019-10-16 12:56:09 +0300

    crud for category

--- SymfonyDemo/src/Controller/CategoryController.php
+++ SymfonyDemo/src/Controller/CategoryController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Category; 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; 
+use Symfony\Component\Routing\Annotation\Route; 
+use Symfony\Component\HttpFoundation\Response;
 
 class CategoryController extends AbstractController
 {
@@ -15,5 +17,78 @@ class CategoryController extends AbstractController
         return $this->render('category/index.html.twig', [
             'controller_name' => 'CategoryController',
         ]);
+    }
+
+    /**
+     * @Route("/createcategory", name="create_category")
+     */
+    public function createCategory():Response
+    {
+        $name = $_GET['name'];
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $category = new Category();
+        $category->setName($name);
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return new Response('Created category with id '.$category->getId());
+    }
+
+    /**
+     * @Route("/category/{id}", name="read_category")
+     */
+    public function readCategory($id)
+    {
+        $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+
+        if(!$category)
+        {
+            throw $this->createNotFoundException('No category found with id='.$id);
+        }
+
+        return new Response('<h1>'.$category->getName());
+    }
+
+    /**
+     * @Route("/updatecategory/{id}", name="update_category")
+     */
+    public function updateCategory($id)
+    {
+        $newName = $_GET['newName'];
+        $entityManager = $this->getDoctrine()->getManager();
+        $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+
+        if(!$category)
+        {
+            throw $this->createNotFoundException('No category found with id='.$id);
+        }
+
+        $category->setName($newName);
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return new Response('<h1>'.$category->getName());
+    }
+
+    /**
+     * @Route("/deletecategory/{id}", name="delete_category")
+     */
+    public function deleteCategory($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $category = $this->getDoctrine()->getRepository(Category::class)->find($id);
+
+        if(!$category)
+        {
+            throw $this->createNotFoundException('No category found with id='.$id);
+        }
+
+        $entityManager->remove($category);
+        $entityManager->flush();
+
+        return new Response('<h1> Deleted category with id='.$id);
     }
 }
