55c2fcd3b3d5c760a778a7cf115742e0fb928800
Emil Marineata <marineataemil@gmail.com>
2019-10-22 18:06:55 +0300

    hai ca merrge

--- SymfonyDemo/src/Controller/AffiliateController.php
+++ SymfonyDemo/src/Controller/AffiliateController.php
@@ -16,4 +16,97 @@ class AffiliateController extends AbstractController
             'controller_name' => 'AffiliateController',
         ]);
     }
+
+    /**
+     * @Route("/createaffiliate", name="create_affiliate")
+     */
+    public function createAffiliate():Response
+    {
+        $name = $_GET['name'];
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $affiliate = new Affiliate();
+        $affiliate->setName($name);
+
+        $entityManager->persist($affiliate);
+        $entityManager->flush();
+
+        return new Response('Created affiliate with id '.$affiliate->getId());
+    }
+
+    /**
+     * @Route("/affiliate/{id}", name="read_affiliate")
+     */
+    public function readAffiliate($id)
+    {
+        $affiliate = $this->getDoctrine()->getRepository(Affiliate::class)->find($id);
+
+        if(!$affiliate)
+        {
+            throw $this->createNotFoundException('No affiliate found with id='.$id);
+        }
+
+        return new Response('<h1>'.$affiliate->getName());
+    }
+
+        /**
+     * @Route("/categories", name="readall_affiliate")
+     */
+    public function readAllCategories()
+    {
+        $categories = $this->getDoctrine()->getRepository(Affiliate::class)->findAll();
+
+        if(!$categories)
+        {
+            throw $this->createNotFoundException('No categories found');
+        }
+
+        $nrOfCategories = count($categories);
+        $responseMsg='';
+        for ($i=0; $i < $nrOfCategories; $i++) { 
+            $responseMsg = $responseMsg.$categories[$i]->getName();
+        }
+
+        return new Response('<h1>'.$responseMsg);
+    }
+
+    /**
+     * @Route("/updateaffiliate/{id}", name="update_affiliate")
+     */
+    public function updateAffiliate($id)
+    {
+        $newName = $_GET['newName'];
+        $entityManager = $this->getDoctrine()->getManager();
+        $affiliate = $this->getDoctrine()->getRepository(Affiliate::class)->find($id);
+
+        if(!$affiliate)
+        {
+            throw $this->createNotFoundException('No affiliate found with id='.$id);
+        }
+
+        $affiliate->setName($newName);
+
+        $entityManager->flush();
+
+        return new Response('<h1>'.$affiliate->getName());
+    }
+
+    /**
+     * @Route("/deleteaffiliate/{id}", name="delete_affiliate")
+     */
+    public function deleteAffiliate($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $affiliate = $this->getDoctrine()->getRepository(Affiliate::class)->find($id);
+
+        if(!$affiliate)
+        {
+            throw $this->createNotFoundException('No affiliate found with id='.$id);
+        }
+
+        $entityManager->remove($affiliate);
+        $entityManager->flush();
+
+        return new Response('<h1> Deleted affiliate with id='.$id);
+    }
 }
--- SymfonyDemo/src/Controller/JobController.php
+++ SymfonyDemo/src/Controller/JobController.php
@@ -16,4 +16,97 @@ class JobController extends AbstractController
             'controller_name' => 'JobController',
         ]);
     }
+     
+    /**
+     * @Route("/createjob", name="create_job")
+     */
+    public function createJob():Response
+    {
+        $name = $_GET['name'];
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $job = new Job();
+        $job->setName($name);
+
+        $entityManager->persist($job);
+        $entityManager->flush();
+
+        return new Response('Created job with id '.$job->getId());
+    }
+
+    /**
+     * @Route("/job/{id}", name="read_job")
+     */
+    public function readJob($id)
+    {
+        $job = $this->getDoctrine()->getRepository(Job::class)->find($id);
+
+        if(!$job)
+        {
+            throw $this->createNotFoundException('No job found with id='.$id);
+        }
+
+        return new Response('<h1>'.$job->getName());
+    }
+
+        /**
+     * @Route("/categories", name="readall_job")
+     */
+    public function readAllCategories()
+    {
+        $categories = $this->getDoctrine()->getRepository(Job::class)->findAll();
+
+        if(!$categories)
+        {
+            throw $this->createNotFoundException('No categories found');
+        }
+
+        $nrOfCategories = count($categories);
+        $responseMsg='';
+        for ($i=0; $i < $nrOfCategories; $i++) { 
+            $responseMsg = $responseMsg.$categories[$i]->getName();
+        }
+
+        return new Response('<h1>'.$responseMsg);
+    }
+
+    /**
+     * @Route("/updatejob/{id}", name="update_job")
+     */
+    public function updateJob($id)
+    {
+        $newName = $_GET['newName'];
+        $entityManager = $this->getDoctrine()->getManager();
+        $job = $this->getDoctrine()->getRepository(Job::class)->find($id);
+
+        if(!$job)
+        {
+            throw $this->createNotFoundException('No job found with id='.$id);
+        }
+
+        $job->setName($newName);
+
+        $entityManager->flush();
+
+        return new Response('<h1>'.$job->getName());
+    }
+
+    /**
+     * @Route("/deletejob/{id}", name="delete_job")
+     */
+    public function deleteJob($id)
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $job = $this->getDoctrine()->getRepository(Job::class)->find($id);
+
+        if(!$job)
+        {
+            throw $this->createNotFoundException('No job found with id='.$id);
+        }
+
+        $entityManager->remove($job);
+        $entityManager->flush();
+
+        return new Response('<h1> Deleted job with id='.$id);
+    }
 }
